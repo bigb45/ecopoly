cef4141e195db1dc778fc5247850bcb3503475bf
Mohammed Natour <bgd4500@gmail.com>
2024-02-17 10:01:31 +0300

    feature: add Property card

---
+++ lib/util/cell_details.dart
@@ -0,0 +1,81 @@
+import 'package:ecopoly/models/cell.dart';
+import 'package:flutter/material.dart';
+
+const cardWidth = 160.0;
+const cardHeight = 200.0;
+const flagSize = 60.0;
+
+class CellDetails extends StatelessWidget {
+  final Cell cell;
+  final VoidCallback onClose;
+  const CellDetails({super.key, required this.cell, required this.onClose});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: cardWidth,
+      height: cardHeight,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 6,
+            offset: const Offset(0, 0),
+          ),
+        ],
+      ),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Positioned(
+            top: -5,
+            right: -5,
+            child: IconButton(
+              icon: const Icon(Icons.close),
+              onPressed: () {
+                onClose();
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: flagSize / 2 + 4),
+            child: Center(
+              child: Column(
+                children: [
+                  Text(cell.name,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      )),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: -flagSize / 2,
+            left: (cardWidth - flagSize) / 2,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(flagSize),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    offset: const Offset(0, 0),
+                  ),
+                ],
+              ),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(flagSize),
+                  child: Image.asset(cell.imageName, height: flagSize)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
